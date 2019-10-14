d22aca891db6685a3db473640dd9acc4aa245ac2
takara1356 <goblin7283@gmail.com>
2019-10-14 21:28:22 +0900

    Add User model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_secure_token
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password_digest, presence: true
+  validates :token, uniqueness: true
+end
