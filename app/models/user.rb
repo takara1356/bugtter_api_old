73df6dc43d6487e996dbcd3f9e118487249e5970
takara1356 <goblin7283@gmail.com>
2019-10-17 23:14:38 +0900

    Add association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token
 
+  has_many :posts
+
   validates :email, presence: true
   validates :email, uniqueness: true
   validates :password_digest, presence: true
