1b5ba98d8ee44cb4e65d2db8a885508360da9589
takara1356 <goblin7283@gmail.com>
2019-10-20 08:36:51 +0900

    add posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :posts
+    end
+  end
+  
+  namespace :api do
+    namespace :v1 do
       resources :users, only: [ :create ] do
         collection do
           post 'sign_in'
