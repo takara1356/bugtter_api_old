ca51e58361ade59be44d9b2721bd00ac05747ad8
takara1356 <goblin7283@gmail.com>
2019-10-30 22:11:11 +0900

    settings for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,43 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.11.2"
+# capistranoのバージョン固定
+lock '3.4.0'
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+# デプロイするアプリケーション名
+set :application, 'bugtter_api'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# cloneするgitのレポジトリ
+set :repo_url, 'git@github.com:takara1356/bugtter_api.git'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# deployするブランチ。デフォルトはmasterなのでなくても可。
+set :branch, 'master'
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+# deploy先のディレクトリ。 
+set :deploy_to, '/var/www/rails/bugtter_api'
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+# シンボリックリンクをはるファイル
+set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
 
-# Default value for :pty is false
-# set :pty, true
+# シンボリックリンクをはるフォルダ
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+# 保持するバージョンの個数
+set :keep_releases, 5
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+# rubyのバージョン
+set :rbenv_ruby, '2.6.3'
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# 出力するログのレベル。
+set :log_level, :debug
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
+namespace :deploy do
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+end
--- config/puma.rb
+++ config/puma.rb
@@ -33,3 +33,12 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+# add setting for production
+_proj_path = "#{File.expand_path("../..", __FILE__)}"
+_proj_name = File.basename(_proj_path)
+_home = ENV.fetch("HOME") { "/home/#{ENV['PUMA_USER']}" }
+pidfile "#{_home}/run/#{_proj_name}.pid"
+bind "unix://#{_home}/run/#{_proj_name}.sock"
+directory _proj_path
+# add end
